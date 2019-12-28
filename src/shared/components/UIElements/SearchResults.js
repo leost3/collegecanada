5475280d0f2a4b364a52743c9269b70a28c39567
leost3 <leost3@gmail.com>
2019-12-28 01:56:15 -0500

    search results component UI crated

---
+++ src/shared/components/UIElements/SearchResults.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SearchResults.scss";
+
+const SearchResults = props => {
+  return <span className="search-results">4/9 provinces found</span>;
+};
+
+export default SearchResults;
