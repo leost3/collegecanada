7ef1b6bac2df43c521f50a7f3e0de9a863a11e16
leost3 <leost3@gmail.com>
2019-12-30 15:07:24 -0500

    fake login logout on clicking button

--- src/shared/components/header/Header.js
+++ src/shared/components/header/Header.js
@@ -7,27 +7,39 @@ import Button from "../FormComponents/Button";
 const Header = () => {
   const [isLoginOpen, setIsLoginOpen] = useState(true);
 
+  const [isUserUthenticated, setIsUserAthenticated] = useState(false);
+
   const toggleLogin = () => {
     setIsLoginOpen(prevLogin => !prevLogin);
   };
+
   const closeLogin = () => {
     setIsLoginOpen(false);
   };
 
   const authenticateUser = e => {
     e.preventDefault();
+    setIsUserAthenticated(true);
     setIsLoginOpen(false);
-    console.log("login");
+  };
+
+  const logout = e => {
+    setIsUserAthenticated(false);
   };
 
   return (
     <div className="main-header">
       <div className="header">
         <span className="logo">logo</span>
-
-        <Button onClick={toggleLogin} className="login-buton">
-          Login
-        </Button>
+        {!isUserUthenticated ? (
+          <Button onClick={toggleLogin} className="login-buton">
+            Login
+          </Button>
+        ) : (
+          <div className="logout" onClick={logout}>
+            Logout
+          </div>
+        )}
         {isLoginOpen && (
           <LoginWindow
             closeLogin={closeLogin}
