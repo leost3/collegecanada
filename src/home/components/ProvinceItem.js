30b59b7a9e0fc5a4a621659e286991def46f4f76
leost3 <leost3@gmail.com>
2020-01-03 17:06:14 -0500

    province cards styled

--- src/home/components/ProvinceItem.js
+++ src/home/components/ProvinceItem.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./ProvinceItem.scss";
 
-const ProvinceItem = props => {
+const ProvinceItem = ({ province }) => {
+  console.log(province);
   return (
     <div className="province-card">
-      <h2>province name</h2>
+      <span className="name">{province.name}</span>
+      <div className="image-container">
+        <img
+          src={require(`../../images/provinces/${province.name}.jpg`)}
+          alt={`${province.name}`}
+        />
+      </div>
+      <div className="capital">
+        <span className="capital-name">{province.capital}</span>
+      </div>
     </div>
   );
 };
--- src/home/components/ProvincesList.js
+++ src/home/components/ProvincesList.js
@@ -1,82 +1,83 @@
 import React from "react";
 
 import ProvinceItem from "./ProvinceItem";
+import Card from "../../shared/components/UIElements/Card/Card";
 
 const ProvicesList = props => {
   const provinces = [
     {
+      id: 0,
       name: "Quebec",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 1,
       name: "Ontario",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 2,
       name: "Alberta",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 3,
       name: "British Columbia",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 4,
       name: "Manitoba",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 5,
       name: "New Brunswick",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
-      name: "Labrator",
-      cities: ["montreal", "quebec city"],
-      capital: "quebec city",
-      population: 2000000
-    },
-    {
-      name: "Labrator",
-      cities: ["montreal", "quebec city"],
-      capital: "quebec city",
-      population: 2000000
-    },
-    {
-      name: "Nova Scotia",
-      cities: ["montreal", "quebec city"],
-      capital: "quebec city",
-      population: 2000000
-    },
-    {
-      name: "Prince Edward Island",
-      cities: ["montreal", "quebec city"],
-      capital: "quebec city",
-      population: 2000000
-    },
-    {
+      id: 8,
       name: "Newfoundland and Labrator",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 9,
+      name: "Nova Scotia",
+      cities: ["montreal", "quebec city"],
+      capital: "quebec city",
+      population: 2000000
+    },
+    {
+      id: 10,
+      name: "Prince Edward Island",
+      cities: ["montreal", "quebec city"],
+      capital: "quebec city",
+      population: 2000000
+    },
+
+    {
+      id: 11,
       name: "Yukon",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
       population: 2000000
     },
     {
+      id: 12,
       name: "Northwest Territories",
       cities: ["montreal", "quebec city"],
       capital: "quebec city",
@@ -84,8 +85,12 @@ const ProvicesList = props => {
     }
   ];
 
-  return provinces.map((province, i) => {
-    return <ProvinceItem key={i} {...props} />;
+  return provinces.map(province => {
+    return (
+      <Card>
+        <ProvinceItem key={province.id} province={province} />
+      </Card>
+    );
   });
 };
 
